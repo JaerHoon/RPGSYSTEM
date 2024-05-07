8496e465de6736ef864c4cd86a64d89baf67ba91
JaerHoon <159917860+JaerHoon@users.noreply.github.com>
2024-05-07 22:25:14 +0900

    Test

--- Assets/Codes/RPGSYSTEM.cs
+++ Assets/Codes/RPGSYSTEM.cs
@@ -5,5 +5,20 @@ using UnityEngine;
 
 namespace RPGSYSTEM
 {
+   public class Skill<T> : MonoBehaviour
+    {
+        T data;
+        int skill_LV=1;
+        
+        public virtual void Init(T data)
+        {
+            this.data = data;
+            skill_LV = 1;
+        }
 
+        public virtual void OnSkill() { }
+        public virtual void Skilling() { }
+        public virtual void OffSkill() { }
+
+    }
 }
